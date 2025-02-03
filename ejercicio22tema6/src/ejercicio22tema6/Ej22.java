0bb953190d56573ebcf0b4dcfab0eb6c15723567
SergioBadia <sergarba16@gmail.com>
2025-02-03 11:56:54 +0100

    He añadido la función mostrarMenu
    Signed-off-by: SergioBadia <sergarba16@gmail.com>

--- ejercicio22tema6/src/ejercicio22tema6/Ej22.java
+++ ejercicio22tema6/src/ejercicio22tema6/Ej22.java
@@ -1,9 +1,17 @@
 package ejercicio22tema6;
 
 public class Ej22 {
+	
+	class Palabra {
+		String termino;
+		String definicion;
+		}
+	
+	static void mostratMenu() {
+		
+	}
 
 	public static void main(String[] args) {
-		
 
 	}
 
